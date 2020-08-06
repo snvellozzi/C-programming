7a2047bdb495bd0a909aaabfa36cc8f14e9f6b41
Sophia Vellozzi <snvello@gmail.com>
2020-08-05 20:19:53 -0400

    Use for loop to print numbers

---
+++ number-loop.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+    printf("The following are the first 10 natural numbers:\n");
+    //prints the first 10 natural numbers
+    for(int i = 1; i <= 10; i++){
+        printf("%d ", i);
+    }
+}
